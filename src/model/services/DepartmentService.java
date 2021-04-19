419d67ce9b61d4b4b048a028273ca1b0b1918a24
Anderson Batista Ferreira da Silva <andersonbfs15@gmail.com>
2021-04-19 09:08:58 -0300

    Saving a new Department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -11,8 +11,15 @@ public class DepartmentService {
 	DepartmentDao dao = DaoFactory.createDepartmentDao();
 	
 	public List<Department> findAll(){
-		
 		return dao.findAll();
 	}
 	
+	public void saveOrUpdate(Department obj) {
+		if(obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
 }
